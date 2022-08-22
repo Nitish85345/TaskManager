bd8c0e2e30ebb4639355d2264a82cb2d635450dd
Nitish <nitishkumar0394@gmail.com>
2022-08-22 13:46:45 +0530

    MongoDB config setup

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI  = "mongodb+srv://nitishkumar0394:85345000@cluster0.o8i3gzr.mongodb.net/tasks?retryWrites=true&w=majority";
+const mongoURI  = "mongodb+srv://nitishkumar0394:85345000@cluster0.o8i3gzr.mongodb.net/taskmanager?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
